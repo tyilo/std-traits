23ab4c0d116621ab31fa32d9cf6079593ba8228f
Asger Hautop Drewsen <asger@tyilo.com>
2024-10-24 18:47:10 +0200

    Add some FunctionPointer tests

--- src/fun.rs
+++ src/fun.rs
@@ -73,3 +73,45 @@ impl_fn!(A1 0, A2 1, A3 2, A4 3, A5 4, A6 5, A7 6, A8 7, A9 8, A10 9, A11 10, A1
 impl_fn!(A1 0, A2 1, A3 2, A4 3, A5 4, A6 5, A7 6, A8 7, A9 8, A10 9, A11 10, A12 11, A13 12, A14 13);
 impl_fn!(A1 0, A2 1, A3 2, A4 3, A5 4, A6 5, A7 6, A8 7, A9 8, A10 9, A11 10, A12 11, A13 12, A14 13, A15 14);
 impl_fn!(A1 0, A2 1, A3 2, A4 3, A5 4, A6 5, A7 6, A8 7, A9 8, A10 9, A11 10, A12 11, A13 12, A14 13, A15 14, A16 15);
+
+#[cfg(test)]
+mod test {
+    use super::FunctionPointer;
+
+    fn f0() {}
+    fn f1<T>(a: T) -> T {
+        a
+    }
+    fn f2<T1, T2>(a: T1, b: T2) -> (T1, T2) {
+        (a, b)
+    }
+    fn f3<T1, T2, T3>(a: T1, b: T2, c: T3) -> (T1, T2, T3) {
+        (a, b, c)
+    }
+
+    #[test]
+    fn test_call_0_args() {
+        (f0 as fn() -> _).call(());
+    }
+
+    #[test]
+    fn test_call_1_arg() {
+        assert_eq!((f1 as fn(usize) -> _).call((1337,)), (1337usize));
+    }
+
+    #[test]
+    fn test_call_2_args() {
+        assert_eq!(
+            (f2 as fn(char, [u8; 2]) -> _).call(('x', [2, 3])),
+            ('x', [2u8, 3u8])
+        );
+    }
+
+    #[test]
+    fn test_call_3_args() {
+        assert_eq!(
+            (f3 as fn(usize, &'static str, bool) -> _).call((1, "b", false)),
+            (1usize, "b", false)
+        );
+    }
+}
