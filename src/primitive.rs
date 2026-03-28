764fb152e46a0949c2526d0b198c32d2f469e049
Asger Hautop Drewsen <asger@tyilo.com>
2026-03-28 12:17:20 +0100

    Make Primitive sealed

--- src/primitive.rs
+++ src/primitive.rs
@@ -1,2 +1,8 @@
 /// A useless(?) trait for all primitive types in Rust.
-pub trait Primitive {}
+pub trait Primitive: private::Sealed {}
+
+mod private {
+    pub trait Sealed {}
+
+    impl<T: ?Sized + super::Primitive> Sealed for T {}
+}
